7bce726fc86bfc0fad86c1b963e7ad2493635315
BimoSora <bimosora3@gmail.com>
2021-07-29 14:37:27 +0700

    first commit

--- index.js
+++ index.js
@@ -437,6 +437,10 @@ bot.command('send',async(ctx)=>{
             const command = words.shift().slice(1);
             const caption = words.join(" ");
 
+            ctx.reply('Terkirim!',{
+                reply_to_message_id: ctx.message.message_id
+            })
+
             bot.telegram.sendMessage(ctx.message.reply_to_message.from.id, `${caption}`)
         }
     }
@@ -525,6 +529,10 @@ bot.command('sendchat',async(ctx)=>{
             const command = words.shift().slice(1);
             const userId = words.shift();
             const caption = words.join(" ");
+
+            ctx.reply('Terkirim!',{
+                reply_to_message_id: ctx.message.message_id
+            })
 
             bot.telegram.sendMessage(userId, `${caption}`)
         }
